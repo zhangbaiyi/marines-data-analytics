99cb35602ea0e64bd18491a4a18e3a7e628d90d7
Andrew Tran <andrewtranva@gmail.com>
2025-02-11 16:37:37 -0500

    Fixed prettier formatting

--- server/e2e/eslint.config.mjs
+++ server/e2e/eslint.config.mjs
@@ -1,35 +1,35 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import js from '@eslint/js';
-import nxEslintPlugin from '@nx/eslint-plugin';
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import js from "@eslint/js";
+import nxEslintPlugin from "@nx/eslint-plugin";
 const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
-  recommendedConfig: js.configs.recommended,
+  recommendedConfig: js.configs.recommended
 });
 
 export default [
-  { plugins: { '@nx': nxEslintPlugin } },
+  { plugins: { "@nx": nxEslintPlugin } },
   ...compat
     .config({
-      extends: ['plugin:@nx/typescript'],
+      extends: ["plugin:@nx/typescript"]
     })
     .map((config) => ({
       ...config,
-      files: ['**/*.ts', '**/*.tsx', '**/*.cts', '**/*.mts'],
+      files: ["**/*.ts", "**/*.tsx", "**/*.cts", "**/*.mts"],
       rules: {
-        ...config.rules,
-      },
+        ...config.rules
+      }
     })),
   ...compat
     .config({
-      extends: ['plugin:@nx/javascript'],
+      extends: ["plugin:@nx/javascript"]
     })
     .map((config) => ({
       ...config,
-      files: ['**/*.js', '**/*.jsx', '**/*.cjs', '**/*.mjs'],
+      files: ["**/*.js", "**/*.jsx", "**/*.cjs", "**/*.mjs"],
       rules: {
-        ...config.rules,
-      },
-    })),
+        ...config.rules
+      }
+    }))
 ];
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,12 @@
-import express from 'express';
+import express from "express";
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const host = process.env["HOST"] ?? "localhost";
+const port = process.env["PORT"] ? Number(process.env["PORT"]) : 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+app.get("/", (_, res) => {
+  res.send({ message: "Hello API" });
 });
 
 app.listen(port, host, () => {
