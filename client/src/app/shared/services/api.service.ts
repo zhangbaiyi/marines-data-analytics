14bce560d3553e86c96d3184e7d77bcc34e47374
Andrew Tran <andrewtranva@gmail.com>
2025-02-11 14:04:17 -0500

    Added an APIService class helper

---
+++ client/src/app/shared/services/api.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { inject, Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+import { APP_CONFIG_TOKEN } from "../../../environments/app-config-env.token";
+
+type APIResponse<T> = T extends (infer U)[] ? { response: U[] } : { response: T };
+
+@Injectable({
+  providedIn: "root"
+})
+export class APIService {
+  private readonly enviornment = inject(APP_CONFIG_TOKEN);
+  private readonly BASE_URL = this.enviornment.API_URL;
+  constructor(private readonly http: HttpClient) {
+    console.log({ enviornment: this.enviornment });
+    console.log({ BASE_URL: this.BASE_URL });
+  }
+
+  get<T>(endpoint: string): Observable<APIResponse<T>> {
+    return this.http.get<APIResponse<T>>(`${this.BASE_URL}/${endpoint}`);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  post<T>(endpoint: string, body: any, options?: { headers?: HttpHeaders }): Observable<APIResponse<T>> {
+    return this.http.post<APIResponse<T>>(`${this.BASE_URL}/${endpoint}`, body, options);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  put<T>(endpoint: string, body: any): Observable<APIResponse<T>> {
+    return this.http.put<APIResponse<T>>(`${this.BASE_URL}/${endpoint}`, body);
+  }
+
+  delete<T>(endpoint: string): Observable<APIResponse<T>> {
+    return this.http.delete<APIResponse<T>>(`${this.BASE_URL}/${endpoint}`);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  patch<T>(endpoint: string, body: any): Observable<APIResponse<T>> {
+    return this.http.patch<APIResponse<T>>(`${this.BASE_URL}/${endpoint}`, body);
+  }
+}
