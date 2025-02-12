1f6240097c748412dec6c52156a63e1ff1eecec6
Andrew Tran <andrewtranva@gmail.com>
2025-02-11 23:41:42 -0500

    Added RabbitMQ service installation Bash script for MacOS users

---
+++ server/rabbitmq_install_macos.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Update Homebrew
+echo "Updating Homebrew..."
+brew update
+
+# Install RabbitMQ
+echo "Installing RabbitMQ..."
+brew install rabbitmq
+
+# Starting RabbitMQ in the background
+echo "Starting RabbitMQ service..."
+brew services start rabbitmq
+
+# Enable all feature flags (recommended)
+echo "Enabling all feature flags on RabbitMQ..."
+/opt/homebrew/sbin/rabbitmqctl enable_feature_flag all
+
+# Verify RabbitMQ installation
+echo "Verifying RabbitMQ installation..."
+brew services list
+rabbitmqctl status
