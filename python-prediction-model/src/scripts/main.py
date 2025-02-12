32fd84ed51c49574961d56e69651f21519fc4d42
Andrew Tran <andrewtranva@gmail.com>
2025-02-11 23:00:10 -0500

    Added RabbitMQ, Logging, and Basic Prediction Logic to 'main' script

--- python-prediction-model/src/scripts/main.py
+++ python-prediction-model/src/scripts/main.py
@@ -1,5 +1,100 @@
-def main():
-    print("Hello World")
+import json
+from dataclasses import dataclass
+from typing import List, NamedTuple
+
+import pika
+import pika.adapters.blocking_connection
+import pika.spec
+
+from src.utils.logging import LOGGER
+
+
+# CONSTANTS
+@dataclass
+class CONSTANTS(NamedTuple):
+    RABBITMQ_URL = "amqp://guest:guest@localhost:5672/"
+    CHANNEL_QUEUE_PREFIX = "file_generate_status"
+    ML_MODEL_FALLBACK_TOKEN_RESULT = "<NO-RESULT>"
+
+
+# TYPES
+class PredictionDict(NamedTuple):
+    prediction: str
+
+
+def predict(contents: str) -> PredictionDict:
+    LOGGER.debug(f"Contents: {contents}")
+    return {"prediction": CONSTANTS.ML_MODEL_FALLBACK_TOKEN_RESULT}
+
+
+def main(
+    rabbitmq_url: str = CONSTANTS.RABBITMQ_URL,
+    queue_prefix: str = CONSTANTS.CHANNEL_QUEUE_PREFIX,
+) -> None:
+    print("Hello World Test:")
+    print("Testing Logging Capabilities")
+
+    # Test logging capabilities (REMOVE LATER)
+    LOGGER.info("Hello World - LOGGING (INFO)")
+    LOGGER.debug("Hello World - LOGGING (DEBUG)")
+    LOGGER.warning("Hello World - LOGGING (WARNING)")
+    LOGGER.error("Hello World - LOGGING (ERROR)")
+    LOGGER.critical("Hello World - LOGGING (CRITICAL)")
+
+    # Connect to RabbitMQ
+    predict_connection = pika.BlockingConnection(pika.URLParameters(rabbitmq_url))
+    channel = predict_connection.channel()
+
+    REQUEST_QUEUE = f"{queue_prefix}_request_queue"
+    RESPONSE_QUEUE = f"{queue_prefix}_response_queue"
+
+    active_queues: List[str] = [REQUEST_QUEUE, RESPONSE_QUEUE]
+    # Ensure that the queues exist (or are created)
+    for queue_name in active_queues:
+        channel.queue_declare(queue=queue_name, durable=True)
+
+    def on_queue_request_received(
+        channel: pika.adapters.blocking_connection.BlockingChannel,
+        method: pika.spec.Basic.Deliver,
+        properties: pika.BasicProperties,
+        body: bytes,
+    ) -> None:
+        global num_request
+
+        # Parse the incoming request
+        request_data: str = json.loads(body)
+        LOGGER.debug(
+            f" [{num_request}] Received Request Number: {properties.correlation_id}"
+        )
+        LOGGER.debug(f" [{num_request}] Received Request Message: {request_data}")
+
+        # Perform the prediction
+        response_data = predict(email_body_contents=request_data)
+        LOGGER.debug(
+            f" [{num_request}] Sending Response Number: {properties.correlation_id}"
+        )
+        LOGGER.debug(f" [{num_request}] Sending Response Message: {response_data}")
+
+        num_request += 1
+
+        # Send the response to the Response Queue
+        channel.basic_publish(
+            exchange="",
+            routing_key=properties.reply_to,
+            body=json.dumps(response_data),
+            properties=pika.BasicProperties(correlation_id=properties.correlation_id),
+        )
+
+        # Acknowledge the request
+        channel.basic_ack(delivery_tag=method.delivery_tag)
+        return None
+
+    # Start consuming from the Request Queue
+    channel.basic_consume(
+        queue=REQUEST_QUEUE, on_message_callback=on_queue_request_received
+    )
+    channel.start_consuming()
+
     return None
 
 
