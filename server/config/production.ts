9de35f1d2c3130603d7689841247495d4997b87a
Andrew Tran <andrewtranva@gmail.com>
2025-02-18 18:59:31 -0500

    Changed 'config' to use index signature/syntax

--- server/config/production.ts
+++ server/config/production.ts
@@ -2,10 +2,10 @@ import { AppConfig } from "./types";
 
 // Necessary Back-End Configuration Properties (using NPM "config")
 const config: AppConfig = {
-  backendServerPort: parseInt(`${process.env.BACKEND_PORT}`),
-  backendServerUrl: `http://localhost:${process.env.BACKEND_PORT}`,
-  frontendClientPort: parseInt(`${process.env.FRONTEND_PORT}`),
-  frontendClientUrl: `http://localhost:${process.env.FRONTEND_PORT}`,
+  backendServerPort: parseInt(`${process.env["BACKEND_PORT"]}`),
+  backendServerUrl: `http://localhost:${process.env["BACKEND_PORT"]}`,
+  frontendClientPort: parseInt(`${process.env["FRONTEND_PORT"]}`),
+  frontendClientUrl: `http://localhost:${process.env["FRONTEND_PORT"]}`,
   logLevel: "trace"
 };
 
