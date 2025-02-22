7e3646cd376101763c16bbf545ba9bb449979725
Andrew Tran <andrewtranva@gmail.com>
2025-02-22 03:06:03 -0500

    Added File-Uploader (Solution #2 Documentation): Ngx-File-Upload

--- client/src/app/components/custom-file-uploader-2/custom-file-uploader-2.component.ts
+++ client/src/app/components/custom-file-uploader-2/custom-file-uploader-2.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
+/**
+ * REFERENCES: Ngx-Awesome-Uploader (File Uploader)
+ * - https://www.npmjs.com/package/@iplab/ngx-file-upload?activeTab=readme
+ * - https://pivan.github.io/file-upload/
+ */
 @Component({
   selector: "custom-file-uploader-2-ngx-file-upload-iplab",
   imports: [CommonModule],
