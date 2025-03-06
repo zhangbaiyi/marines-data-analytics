6870944182c2e108bd133c6bcd3c13fb8c6d6687
zhangbaiyi <baiyi@vt.edu>
2025-03-06 02:28:56 -0500

    add: sqlalchemy orm for warehouse tables

---
+++ python-prediction-model/src/scripts/data_warehouse/models/warehouse.py
@@ -0,0 +1,141 @@
+import os
+from typing import List, Optional
+from src.utils.logging import LOGGER
+from sqlalchemy import (
+    Boolean,
+    CheckConstraint,
+    Date,
+    DateTime,
+    ForeignKey,
+    String,
+    Float,
+    create_engine,
+)
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+    sessionmaker,
+)
+from datetime import datetime
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "..", "..", "..", "db", "database.sqlite3")
+db_path = os.path.normpath(db_path)
+
+engine = create_engine(f"sqlite:///{db_path}", echo=True)
+Session = sessionmaker(bind=engine)
+
+class Base(DeclarativeBase):
+    pass
+
+class Sites(Base):
+    __tablename__ = "sites"
+
+    __table_args__ = (
+        CheckConstraint(
+            "store_type IN ('Main Store', 'Marine Mart')",
+            name="chk_store_type",
+        ),
+    )
+
+    site_id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    site_name: Mapped[Optional[str]] = mapped_column(String(50))
+    command_name: Mapped[Optional[str]] = mapped_column(String(30))
+    store_type: Mapped[str] = mapped_column(String(20), nullable=False)
+
+    def __repr__(self) -> str:
+        return (
+            f"Sites(site_id={self.site_id!r}, "
+            f"site_name={self.site_name!r}, "
+            f"command_name={self.command_name!r}, "
+            f"store_type={self.store_type!r})"
+        )
+
+
+class Metrics(Base):
+    __tablename__ = "metrics"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    metric_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    metric_desc: Mapped[Optional[str]] = mapped_column(String(200))
+    is_retail: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_marketing: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_survey: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_daily: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_monthly: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_quarterly: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_yearly: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    facts: Mapped[List["Facts"]] = relationship(
+        back_populates="metric", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return (
+            f"Metrics(id={self.id!r}, "
+            f"metric_name={self.metric_name!r}, "
+            f"metric_desc={self.metric_desc!r})"
+        )
+
+
+class PeriodDim(Base):
+    __tablename__ = "period_dim"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    period_name: Mapped[str] = mapped_column(String(15), nullable=False)
+
+    def __repr__(self) -> str:
+        return f"PeriodDim(id={self.id!r}, " f"period_name={self.period_name!r})"
+
+
+class Facts(Base):
+    __tablename__ = "facts"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    metric_id: Mapped[Optional[int]] = mapped_column(ForeignKey("metrics.id"))
+    group_name: Mapped[Optional[str]] = mapped_column(String(50))
+    value: Mapped[Optional[float]] = mapped_column(Float)
+    date: Mapped[Optional[datetime]] = mapped_column(Date)
+    period_level: Mapped[Optional[int]] = mapped_column()
+    record_inserted_date: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow
+    )
+
+    metric: Mapped[Optional[Metrics]] = relationship("Metrics", back_populates="facts")
+
+    def __repr__(self) -> str:
+        return (
+            f"Facts(id={self.id!r}, metric_id={self.metric_id!r}, "
+            f"group_name={self.group_name!r}, value={self.value!r}, "
+            f"date={self.date!r}, period_level={self.period_level!r}, "
+            f"record_inserted_date={self.record_inserted_date!r})"
+        )
+
+
+if __name__ == "__main__":
+
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    LOGGER.info(f"Base dir - {BASE_DIR}")
+    db_path = os.path.join(BASE_DIR, "..", "..", "..", "db", "database.sqlite3")
+    db_path = os.path.normpath(db_path)
+    LOGGER.info(f"DB path - {db_path}")
+
+    engine = create_engine(f"sqlite:///{db_path}", echo=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_metric = Metrics(
+        metric_name="Daily Sales", metric_desc="Track daily sales", is_daily=True
+    )
+    session.add(new_metric)
+    session.commit()
+    LOGGER.info(f"Inserted metric with ID {new_metric.id}")
+
+    metric = session.query(Metrics).filter_by(metric_name="Daily Sales").first()
+    LOGGER.info(f"Fetched metric: {metric}")
+
+    session.close()
