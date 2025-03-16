8999cd31f2b4bfb3a8587407f8e62b0bf4b5d04d
zhangbaiyi <baiyi@vt.edu>
2025-03-16 01:37:03 -0400

    add: general test file

--- python-prediction-model/src/scripts/data_warehouse/test.py
+++ python-prediction-model/src/scripts/data_warehouse/test.py
@@ -1,5 +1,5 @@
 from datetime import date
-from src.scripts.data_warehouse.access import query_facts
+from src.scripts.data_warehouse.access import convert_jargons, getMetricByID, getSiteByID, query_facts
 from src.utils.logging import LOGGER
 
 from src.scripts.data_warehouse.models.warehouse import Session
@@ -28,6 +28,13 @@ if __name__ == "__main__":
                                     HHM MCX MAIN STORE (Henderson Hall Main Store) 
                                         On Jan 1st 2025? """)
     LOGGER.info(query_facts(session=session, metric_ids=[1,2], group_names=['1100'], period_levels=[1], exact_date=date(2025,1,1)))
+
+    LOGGER.info(getMetricByID(session=session, metric_id=1))
+    LOGGER.info(getSiteByID(session = session, site_id='1100'))
+    LOGGER.info(convert_jargons(session = session, df = query_facts(session=session, metric_id=1, group_names=['1100'], period_levels=[2], exact_date=date(2024,12,1))))
+    LOGGER.info(convert_jargons(session = session, df = query_facts(session=session, metric_id=1, group_names=['1100'], period_levels=[1], date_from=date(2025,1,1), date_to=date(2025,1,3))))
+    LOGGER.info(convert_jargons(session = session, df = query_facts(session=session, metric_id=1, group_names=['1100'], period_levels=[3], exact_date=date(2024,10,1))))
+    LOGGER.info(convert_jargons(session = session, df = query_facts(session=session, metric_ids=[1], group_names=['1100', '5206', '2301'], period_levels=[4], exact_date=date(2025,1,1))))
 
 
 # # [Facts(metric_id=('Total Sales, Total sales of Marine Mart or Main Store'), group_name='HHM MCX MAIN STORE,HENDERSON HALL,MAIN STORE', value=2224948.54, 20241201->20241231]
