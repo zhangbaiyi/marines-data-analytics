f891e884545011248350140659f954176808b711
Andrew Tran <andrewtranva@gmail.com>
2025-03-18 00:13:23 -0400

    Added line-length argument for isort

--- python-prediction-model/python_format.sh
+++ python-prediction-model/python_format.sh
@@ -6,6 +6,6 @@
 pip3 install isort black flake8 autopep8
 
 flake8 --max-line-length 120 *.py src/**/*.py
-isort *.py src/**/*.py 
+isort *.py src/**/*.py --line-length 120
 black *.py src/**/*.py --line-length 120
 find src/ -name '*.py' -exec autopep8 --in-place '{}' \;
